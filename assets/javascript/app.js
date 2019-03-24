ef0aef4e3eb0d59df752b58d0e7a1879aa49369e
Ryyder <brucecho82@hotmail.com>
2019-03-24 00:00:30 -0700

    updated js html file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,24 +4,23 @@ $(document).ready(function () {
   var incorrect = 0; //hold incorrect answer value
   var counter = 0; //holds question position
   var innerLength = 0;
-  var outerLength = 0;
   var questionSec = 10;
   var transitionSec = 5;
   var questionID;
   var transitionID;
 
-
   //structure to hold trivia questions, choices, and answers
   var triviaQuestions = [
     {
       question: "Which saga did Goku attain Super Saiyan 3?",
-       choice: [
+      choice: [
         "Frieza Saga",
         "Cell Saga",
         "Buu Saga",
         "Tournament of Power"
       ],
-      answer: 2
+      answer: 2,
+      img: "./assets/images/buusaga.jpg"
     },
      {
       question: "What planet did the saiyans come from?",
@@ -31,7 +30,8 @@ $(document).ready(function () {
         "Earth",
         "Prison Planet"
      ],
-      answer: 0
+      answer: 0,
+      img: "./assets/images/planetvegeta.jpg"
     },
     {
       question: "Who cut Vegeta's tail off?",
@@ -41,7 +41,8 @@ $(document).ready(function () {
         "Krillin",
         "Yajirobe"
       ],
-      answer: 3
+      answer: 3,
+      img: "./assets/images/yajirobe"
     }, 
     {
       question: "How many Dragon Balls are needed to make a wish?",
@@ -51,7 +52,8 @@ $(document).ready(function () {
         "7",
         "10"
       ],
-      answer: 2
+      answer: 2,
+      img: "./assets/images/dragonballs.jpg"
     },
     {
       question: "Who is Goku's father?",
@@ -61,7 +63,8 @@ $(document).ready(function () {
         "Broly",
         "King Vegeta"
       ],
-      answer: 0
+      answer: 0,
+      img: "./assets/images/bardock.png"
     },
     {
       question: "What body part is Future Gohan missing?",
@@ -71,7 +74,8 @@ $(document).ready(function () {
         "Ear",
         "Arm"
       ],
-      answer: 3
+      answer: 3,
+      img: "./assets/images/futuregohan.png"
     },
     {
       question: "Who did Trunks & Goten disguise themselves as while fighting Android 18?",
@@ -81,7 +85,8 @@ $(document).ready(function () {
         "Mystery Mask",
         "Great Saiyman"
       ],
-      answer: 1
+      answer: 1,
+      img: "./assets/images/mightymask.png"
     },
     {
       question: "Who uses the 'Wolf Fang Fist' technique?",
@@ -91,7 +96,8 @@ $(document).ready(function () {
         "Krillin",
         "Tien"
       ],
-      answer: 1
+      answer: 1,
+      img: "./assets/images/yamcha.jpg"
     },
     {
       question: "Time traveler that came from the future?",
@@ -101,7 +107,8 @@ $(document).ready(function () {
         "Vegeta",
         "Dende"
       ],
-      answer: 0
+      answer: 0,
+      img: "./assets/images/futuretrunks.jpg"
     },
     {
       question: "Who is Vegeta's wife?",
@@ -111,29 +118,56 @@ $(document).ready(function () {
         "Videl",
         "Bulma"
       ],
-      answer: 3
+      answer: 3,
+      img: "./assets/images/bulma.jpg"
     }
   ];
 
-  /* console.log("Questions: " + triviaQuestions[0].question);
-  console.log("choices: " + triviaQuestions[0].choice);
-  console.log("Correct Answer: " + triviaQuestions[0].answer); */
-  console.log("choices: " + triviaQuestions[0].answer);
+  console.log("Question 2 answer: " + triviaQuestions[1].answer);
+  console.log("You guessed: " + triviaQuestions[1].choice[0]);
+  console.log("correct answer: " + triviaQuestions[1].answer);
+  console.log("image src: " + triviaQuestions[0].img);
+
 
   innerLength = triviaQuestions[0].choice.length;
   outerLength = triviaQuestions.length;
 
-  console.log(triviaQuestions[counter].question);
+  //console.log(triviaQuestions[counter].question);
   
   //generate question function
   function showQuestion(num) {
+
       //output question and possible choices
-      $("#question").html("<h1>" + triviaQuestions[num].question + "</h1>");
+      $("#question").html("<h2>" + triviaQuestions[num].question + "</h2>");
+
       for (var j = 0; j < innerLength; j++) {
-        //output the choices
-        $("#choice").append("<p>Choices: " + triviaQuestions[num].choice[j] + "</p>");
-      }     
+
+        //output choices to the user
+        var button = $("<button>" + triviaQuestions[num].choice[j] + "</button>" + "<br>");
+
+        button.addClass("user-choice");
+        
+        //append button values to our choice div
+        $("#choice").append(button);
+
+        //each choice div will have a value, that correlates to it's position in our triviaquestion.choice object array
+        button.attr("value", j);
+      }
   }
+
+  $(document).on("click", ".user-choice", function() {
+    
+    //extract the value from the button user clicks
+    var userChoice = ($(this).attr("value"));
+
+    //convert to integer since this was originally a string
+    userChoice = parseInt(userChoice);
+
+    console.log(userChoice);
+
+
+
+  });
 
   /* $("#choice").html("<p>Choices: " + triviaQuestions[0].choice[0]+ "</p>");
   $("#choice").html("<p>Choices: " + triviaQuestions[0].choice[1]+ "</p>"); */
@@ -143,14 +177,17 @@ $(document).ready(function () {
   //clear choice function
   function reInit() {
     counter++;
-    questionSec = 30;
+    questionSec = 10;
     transitionSec = 5;
-    $("#choice").empty();
+    //$("#choice").empty();
     $("#msg").empty();
   }
 
-  function timeMsg() {
+  function timeMsg(num) {
     $("#msg").append("Out of Time!");
+    var choiceImage = $("<img>");
+    choiceImage.attr("src", triviaQuestions[num].img);
+    $("#correct-image").empty().append(choiceImage);
   }
 
   //starts my timer
@@ -166,13 +203,11 @@ $(document).ready(function () {
     $("#show-time").html("<h2> Time Remaining: " + questionSec + "</h2>");
 
     if (questionSec === 0) {
+      incorrect++;
       questionStop();
-      timeMsg();
-      //reInit();
+      timeMsg(counter);
+      $("#choice").empty();
       transitionStart();
-      //showQuestion(counter);
-      //questionStart();
-      //alert("Time Up!");
     }
   }
   
@@ -190,7 +225,7 @@ $(document).ready(function () {
 
     if(transitionSec === 0) {
       transitionStop();
-      timeMsg();
+      $("#correct-image").empty();
       reInit();
       showQuestion(counter);
       questionStart();
@@ -218,5 +253,7 @@ $(document).ready(function () {
 
   }
 
-  //
+  //correct answer: if value of user click === to triviaQuestion[counter].answer update correct++, and show correct answer. don't forget to parseInt the value from html page
+
+  //incorrect answer: if value of user click != to triviaQuestion[counter].answer update incorrect, and show correct answer. don't forget to parseInt the value from the html page
 });
