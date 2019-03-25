3f4c47ec0b84da479992c909d8585db5f41f5e70
Ryyder <brucecho82@hotmail.com>
2019-03-24 18:25:37 -0700

    updated js and html file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,9 @@ $(document).ready(function () {
   var transitionSec = 5;
   var questionID;
   var transitionID;
+  var isCorrect = false;
+  var isIncorrect = false;
+  var isTimeUp = false;
 
   //structure to hold trivia questions, choices, and answers
   var triviaQuestions = [
@@ -22,14 +25,14 @@ $(document).ready(function () {
       answer: 2,
       img: "./assets/images/buusaga.jpg"
     },
-     {
+    {
       question: "What planet did the saiyans come from?",
       choice: [
         "Planet Vegeta",
         "Namek",
         "Earth",
         "Prison Planet"
-     ],
+      ],
       answer: 0,
       img: "./assets/images/planetvegeta.jpg"
     },
@@ -43,7 +46,7 @@ $(document).ready(function () {
       ],
       answer: 3,
       img: "./assets/images/yajirobe"
-    }, 
+    },
     {
       question: "How many Dragon Balls are needed to make a wish?",
       choice: [
@@ -133,30 +136,41 @@ $(document).ready(function () {
   outerLength = triviaQuestions.length;
 
   //console.log(triviaQuestions[counter].question);
-  
+
   //generate question function
   function showQuestion(num) {
 
-      //output question and possible choices
-      $("#question").html("<h2>" + triviaQuestions[num].question + "</h2>");
+    /* for (var i = 0; i < triviaQuestions.length; i++) {
+      
+      $("#question").append("<h2>" + triviaQuestions[i].question + "</h2>")
 
-      for (var j = 0; j < innerLength; j++) {
+      for (var j = 0; j < triviaQuestions[i].choice.length; j++) {
 
-        //output choices to the user
-        var button = $("<button>" + triviaQuestions[num].choice[j] + "</button>" + "<br>");
-
-        button.addClass("user-choice");
-        
-        //append button values to our choice div
-        $("#choice").append(button);
-
-        //each choice div will have a value, that correlates to it's position in our triviaquestion.choice object array
-        button.attr("value", j);
+        $("#question").append(`<input type = "radio" name = ${i} value = ${j}>${triviaQuestions[i].choice[j]}</input>`)
       }
+    } */
+
+    //output question and possible choices
+    $("#question").html("<h2>" + triviaQuestions[num].question + "</h2>");
+
+    for (var j = 0; j < innerLength; j++) {
+
+      //output choices to the user
+      var button = $("<button>" + triviaQuestions[num].choice[j] + "</button>" + "<br>");
+
+      button.addClass("user-choice");
+
+      //append button values to our choice div
+      $("#choice").append(button);
+
+      //each choice div will have a value, that correlates to it's position in our triviaquestion.choice object array
+      button.attr("value", j);
+    }
+
   }
 
-  $(document).on("click", ".user-choice", function() {
-    
+  $(document).on("click", ".user-choice", function () {
+
     //extract the value from the button user clicks
     var userChoice = ($(this).attr("value"));
 
@@ -165,13 +179,70 @@ $(document).ready(function () {
 
     console.log(userChoice);
 
+    if (userChoice === triviaQuestions[counter].answer) {
+      correct++;
+      isCorrect = true;
+      questionStop();
+      timeMsg(counter);
+      $("#choice").empty();
+      transitionStart();
+    }
+    else if (userChoice != triviaQuestions[counter].answer) {
+      incorrect++;
+      isIncorrect = true;
+      questionStop();
+      timeMsg(counter);
+      $("#choice").empty();
+      transitionStart();
+    }
+    console.log("correct answer: " + correct);
+    console.log("incorrect answer: " + incorrect);
+    console.log("counter: " + counter);
+    console.log("isCorrect: " + isCorrect);
+    console.log("inIncorrect: " + isIncorrect);
+
+  });
+
+
+
+  $("#submit-quiz").on("click", function (event) {
+
+    event.preventDefault();
+
+    /* var q1 = $('input[name=0]:checked').val();
+ 
+     var q2 = $('input[name=1]:checked').val();
+ 
+     var q3 = $('input[name=2]:checked').val();
+ 
+     var q4 = $('input[name=3]:checked').val();
+ 
+     var q5 = $('input[name=4]:checked').val();
+ 
+     var q6 = $('input[name=5]:checked').val();
+ 
+     var q7 = $('input[name=6]:checked').val();
+ 
+     var q8 = $('input[name=7]:checked').val();
+ 
+     var q9 = $('input[name=8]:checked').val();
+ 
+     var q10 = $('input[name=9]:checked').val();
+ 
+     var userSubmit = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
+     console.log(userSubmit); 
+ 
+      for (var i = 0; i < userSubmit.length; i++) {
+       if (userSubmit[i] == correctAnswers[i]) {
+         correct++;
+       }
+     } 
+ 
+     $("#msg").append(`<h2>${correct}</h2>`); */
 
 
   });
 
-  /* $("#choice").html("<p>Choices: " + triviaQuestions[0].choice[0]+ "</p>");
-  $("#choice").html("<p>Choices: " + triviaQuestions[0].choice[1]+ "</p>"); */
-  //console.log(counter);
   showQuestion(counter);
 
   //clear choice function
@@ -179,38 +250,68 @@ $(document).ready(function () {
     counter++;
     questionSec = 10;
     transitionSec = 5;
+    isCorrect = false;
+    isIncorrect = false;
+    isTimeUp = false;
     //$("#choice").empty();
     $("#msg").empty();
+    //$("#correct-image").empty();
   }
 
+  //function to handle image output and message output to user...
   function timeMsg(num) {
-    $("#msg").append("Out of Time!");
+
     var choiceImage = $("<img>");
-    choiceImage.attr("src", triviaQuestions[num].img);
-    $("#correct-image").empty().append(choiceImage);
+
+    //handles if the user chooses correct answer
+    if (isCorrect == true) {
+
+      $("#msg").append("Correct Answer!");
+      //$("#correct-answer").append(triviaQuestions[num].answer);
+      choiceImage.attr("src", triviaQuestions[num].img);
+      $("#correct-image").empty().append(choiceImage);
+    }
+    //handles if the user chooses incorrect answer
+    else if (isIncorrect == true) {
+
+      $("#msg").append("Incorrect Answer!");
+      //$("#correct-answer").append(triviaQuestions[num].answer);
+      choiceImage.attr("src", triviaQuestions[num].img);
+      $("#correct-image").empty().append(choiceImage);
+    }
+    //handles if the user time runs out
+    else if (isTimeUp == true) {
+
+      $("#msg").append("Out of Time!");
+      //$("#correct-answer").append(triviaQuestions[num].answer);
+      choiceImage.attr("src", triviaQuestions[num].img);
+      $("#correct-image").empty().append(choiceImage);
+
+    }
   }
 
   //starts my timer
-   function questionStart() {
+  function questionStart() {
     questionID = setInterval(questionTimer, 1000);
-  } 
+  }
 
   //function question timer
-   function questionTimer() {
-    
+  function questionTimer() {
+
     questionSec--;
 
     $("#show-time").html("<h2> Time Remaining: " + questionSec + "</h2>");
 
     if (questionSec === 0) {
       incorrect++;
+      isTimeUp = true;
       questionStop();
       timeMsg(counter);
       $("#choice").empty();
       transitionStart();
     }
   }
-  
+
   function transitionStart() {
     transitionID = setInterval(transitionTimer, 1000);
 
@@ -223,7 +324,7 @@ $(document).ready(function () {
 
     console.log("transition time: " + transitionSec);
 
-    if(transitionSec === 0) {
+    if (transitionSec === 0) {
       transitionStop();
       $("#correct-image").empty();
       reInit();
@@ -246,14 +347,10 @@ $(document).ready(function () {
 
   questionStart();
 
-  //reset game function
-  function resetGame() {
 
 
 
-  }
+  //}
 
-  //correct answer: if value of user click === to triviaQuestion[counter].answer update correct++, and show correct answer. don't forget to parseInt the value from html page
 
-  //incorrect answer: if value of user click != to triviaQuestion[counter].answer update incorrect, and show correct answer. don't forget to parseInt the value from the html page
 });
